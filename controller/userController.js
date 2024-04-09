2d3205a940c2bc28d3e6de1a68f2ab6aa00d37f2
Ravin Yadav <ravinyadav973@gmail.com>
2024-04-09 13:13:02 +0530

    login api created

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,4 @@
+const generateToken = require("../config/jwtToken");
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 
@@ -17,6 +18,24 @@ const createUser = asyncHandler(async(req,res) => {
     }
 })
 
+
+const loginUser = asyncHandler(async (req,res) => {
+    const {email, password} = req.body;
+    //check if user exists or not
+    const findUser = await User.findOne({email});
+    if(findUser && await findUser.isPasswordMatched(password)) {
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id)
+        });
+    } else {
+        throw new Error("Credential Invalid");
+    }
+})
  
 
-module.exports = {createUser};
+module.exports = {createUser,loginUser};
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose'); // Erase if already required
+const bcrypt = require("bcrypt");
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
@@ -20,11 +21,27 @@ var userSchema = new mongoose.Schema({
         required:true,
         unique:true,
     },
-    password:{
+    password:{ 
         type:String,
         required:true,
     },
+    role: {
+        type: String,
+        default:"user"
+    }
 });
+
+
+userSchema.pre('save', async function(next) {
+const salt = await bcrypt.genSaltSync(10);
+this.password = await bcrypt.hash(this.password,salt)
+
+})
+
+
+userSchema.methods.isPasswordMatched = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
 
 //Export the model
 module.exports = mongoose.model('User', userSchema);
