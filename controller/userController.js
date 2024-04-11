946657f6b0ef5588e6f98e331b6eb136a271488c
Ravin Yadav <ravinyadav973@gmail.com>
2024-04-11 17:33:18 +0530

    block unblock user api created

--- controller/userController.js
+++ controller/userController.js
@@ -2,131 +2,168 @@ const generateToken = require("../config/jwtToken");
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 
-
-
-
 //Create a user
 
-const createUser = asyncHandler(async(req,res) => {
-    const email = req.body.email;
-    const findUser = await User.findOne({email});
-    if(!findUser) {
-        const newUser = await User.create(req.body);
-        res.json({
-            msg: "User created successfully",
-            success: true,
-            newUser
-        });
-    } else {
-        throw new Error("User Already Exists")
-    }
-})
-
-
+const createUser = asyncHandler(async (req, res) => {
+  const email = req.body.email;
+  const findUser = await User.findOne({ email });
+  if (!findUser) {
+    const newUser = await User.create(req.body);
+    res.json({
+      msg: "User created successfully",
+      success: true,
+      newUser,
+    });
+  } else {
+    throw new Error("User Already Exists");
+  }
+});
 
 //Login user
 
-const loginUser = asyncHandler(async (req,res) => {
-    const {email, password} = req.body;
-    //check if user exists or not
-    const findUser = await User.findOne({email});
-    if(findUser && await findUser.isPasswordMatched(password)) {
-        res.json({
-            _id: findUser?._id,
-            firstname: findUser?.firstname,
-            lastname: findUser?.lastname,
-            email: findUser?.email,
-            mobile: findUser?.mobile,
-            token: generateToken(findUser?._id)
-        });
-    } else {
-        throw new Error("Credential Invalid");
-    }
-})
-
-
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  //check if user exists or not
+  const findUser = await User.findOne({ email });
+  if (findUser && (await findUser.isPasswordMatched(password))) {
+    res.json({
+      _id: findUser?._id,
+      firstname: findUser?.firstname,
+      lastname: findUser?.lastname,
+      email: findUser?.email,
+      mobile: findUser?.mobile,
+      token: generateToken(findUser?._id),
+    });
+  } else {
+    throw new Error("Credential Invalid");
+  }
+});
 
 //get all user
 
-const getUsers = asyncHandler(async(req,res) => {
-    try {
-        const users = await User.find();
-        res.json({
-            success:true,
-            msg: "Users Fetched",
-            users
-        })
-    } catch (error) {
-        throw new Error(error);
-    }
-})
-
-
+const getUsers = asyncHandler(async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json({
+      success: true,
+      msg: "Users Fetched",
+      users,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 //get a user
 
-const getUser = asyncHandler(async(req,res) => {
-    try {
-        const {id} = req.params;
-        const user = await User.findById(id)
-        if(!user) {
-            res.json({
-                success: false,
-                msg: "user not found",
-            })
-        }
-        res.json({
-            success: true,
-            msg: "user fetched",
-            user
-        })
-    } catch (error) {
-        throw new Error(error)
+const getUser = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      res.json({
+        success: false,
+        msg: "user not found",
+      });
     }
-})
-
-
+    res.json({
+      success: true,
+      msg: "user fetched",
+      user,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 //delete a user
 
-const deleteUser = asyncHandler(async(req,res) => {
-   try {
-    const {id} = req.params;
+const deleteUser = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
     const deleteuser = await User.findByIdAndDelete(id);
     res.json({
-        success: true,
-        msg: "User deleted"
-    })
-   } catch (error) {
-    throw new Error(error)
-   }
-})
-
-
+      success: true,
+      msg: "User deleted",
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 //update a User
 
-const updateUser = asyncHandler(async (req,res) => {
-    const {id} = req.params;
-   try {
-    const updateduser = await User.findByIdAndUpdate(id,{
+const updateUser = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  try {
+    const updateduser = await User.findByIdAndUpdate(
+      id,
+      {
         firstname: req?.body?.firstname,
         lastname: req?.body?.lastname,
         email: req?.body?.email,
         mobile: req?.body?.mobile,
-    },
-    {new: true}
-);
+      },
+      { new: true }
+    );
     res.json({
-        success: true,
-        msg: "user updated",
-        updateduser
-    })
-   } catch (error) {
-    throw new Error(error)
-   }
-})
+      success: true,
+      msg: "user updated",
+      updateduser,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
- 
+const blockUser = asyncHandler(async (req, res) => {
+    console.log(req.params);
+    const {id} = req.params;
+    try {
+        const block = await User.findByIdAndUpdate(id,
+            {
+                isBlocked: true
+            },
+            {
+                new: true
+            }
+        );
+        res.json({
+            success: true,
+            msg: "user blocked"
+        })
+    } catch (error) {
+       throw new Error(error) 
+    }
+});
 
-module.exports = {createUser,loginUser, getUsers,getUser, deleteUser, updateUser};
+const unBlockUser = asyncHandler(async (req, res) => {
+    console.log(req.params);
+    try {
+        const block = await User.findByIdAndUpdate(id,
+            {
+                isBlocked: false
+            },
+            {
+                new: true
+            }
+        );
+        res.json({
+            success:true,
+            msg: "user unblocked"
+        })
+    } catch (error) {
+       throw new Error(error) 
+    }
+});
+
+module.exports = {
+  createUser,
+  loginUser,
+  getUsers,
+  getUser,
+  deleteUser,
+  updateUser,
+  blockUser,
+  unBlockUser,
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,24 @@
 const express = require("express");
-const { createUser, loginUser, getUsers, getUser, deleteUser, updateUser } = require("../controller/userController");
-const {authMiddleware, isAdmin}= require("../middlewares/authMiddleware");
+const {
+  createUser,
+  loginUser,
+  getUsers,
+  getUser,
+  deleteUser,
+  updateUser,
+  blockUser,
+  unBlockUser,
+} = require("../controller/userController");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-
-router.post("/register",createUser);
+router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/get-users", getUsers);
-router.get("/:id",authMiddleware,isAdmin,getUser);
+router.get("/:id", authMiddleware, isAdmin, getUser);
 router.delete("/:id", deleteUser);
-router.put("/:id",updateUser)
-
+router.put("/updateuser", authMiddleware, updateUser);
+router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
+router.put("/unblock-user/:id", authMiddleware, isAdmin, unBlockUser);
 
 module.exports = router;
